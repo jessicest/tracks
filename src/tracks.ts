91bcb6de1d877932cf8ed869d9f976a38e6c7703
Jessica Knight <corence@gmail.com>
2020-10-25 18:20:26 +1100

    cool! apparently we just solved an 8x8 easy puzzle too.
    i haven't verified it but the program seems pretty confident.

--- src/tracks.ts
+++ src/tracks.ts
@@ -540,8 +540,11 @@ function describe_grid(grid: Grid): string {
 }
 
 function main() {
-    console.log("omg\n");
-    //static make_grid(cx: Index, cy: Index, live_links: Array<[Pos, Direction]>, hints: Array<[Index, Direction]>): Grid {
+    function link(x: Index, y: Index, south: boolean): LinkId {
+        return { pos: { x, y }, direction: south ? Direction.South : Direction.East };
+    }
+
+    /*
     const grid = make_grid(4, 4, [
             { pos: { x: 1, y: 1 }, direction: Direction.South },
             { pos: { x: 0, y: 2 }, direction: Direction.East },
@@ -551,12 +554,35 @@ function main() {
         [4,3,3,2],
         [4,3,3,2]
     );
-    //console.log(util.inspect(grid, { depth: 6 }));
+    */
+
+    // 8x8:n9a5a3g5a9k3i5hCd,7,8,8,S7,6,4,5,2,8,7,S5,6,7,5,5,4
+    const grid = make_grid(8, 8, [
+            link(0, 3, false),
+            link(1, 3, false),
+            link(1, 6, false),
+            link(2, 4, false),
+            link(2, 7, false),
+            link(3, 3, false),
+            link(3, 4, false),
+            link(3, 7, false),
+            link(3, 8, false),
+            link(5, 4, false),
+            link(7, 2, false),
+            link(1, 5, true),
+            link(3, 2, true),
+            link(4, 8, true),
+            link(5, 4, true),
+            link(7, 2, true)
+        ],
+        [7,8,8,7,6,4,5,2],
+        [8,7,5,6,7,5,5,4]
+    );
+
     console.log(describe_grid(grid));
 
     grid.solve();
 
-    //console.log(util.inspect(grid, { depth: 6 }));
     console.log();
     console.log(describe_grid(grid));
 }
